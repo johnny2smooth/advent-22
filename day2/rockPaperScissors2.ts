92102f295e24fc7c8fdd8ecbbdb3a5f5392d04c0
johnny2smooth <johnbp13@gmail.com>
2022-12-03 08:16:35 -0800

    pt 2 solved

--- day2/rockPaperScissors2.ts
+++ day2/rockPaperScissors2.ts
@@ -1,28 +1,35 @@
 const input = await Deno.readTextFile("day2/input.txt");
 const cleanInput: string[] = input.replaceAll(" ", "").split(/\n/)
 
-export type rockPaperScissors = "Rock" | "Paper" | "Scissors";
+export type rockPaperScissors = "Rock" | "Paper" | "Scissors" | "Lose" | "Draw" | "Win";
 
 export type ElfEncryption = { A: rockPaperScissors, B: rockPaperScissors, C: rockPaperScissors, X: rockPaperScissors, Y: rockPaperScissors, Z: string }
+export type Moves = { Rock: string, Paper: string, Scissors: string }
 export type Score = { Rock: number, Paper: number, Scissors: number }
 
-const elfValues: ElfEncryption = { "A": "Rock", "B": "Paper", "C": "Scissors", "X": "Rock", "Y": "Paper", "Z": "Scissors" }
-const shapeScore: Score = { "Rock": 1, "Paper": 2, "Scissors": 3 }
+const elfValues: ElfEncryption = { A: "Rock", B: "Paper", C: "Scissors", X: "Lose", Y: "Draw", Z: "Win" }
+const Lose: Moves = { "Rock": "Scissors", "Paper": "Rock", "Scissors": "Paper" };
+const Win: Moves = { "Rock": "Paper", "Paper": "Scissors", "Scissors": "Rock" };
+const shapeScore: Score = { "Rock": 1, "Paper": 2, "Scissors": 3 };
+
 
 let totalScore = 0;
 
 cleanInput.map(([elf, me]: string) => {
   const elfMove = elfValues[elf as keyof ElfEncryption];
-  const myMove = elfValues[me as keyof ElfEncryption];
-  if (elfMove === myMove) {
-    totalScore += 3 + shapeScore[myMove as keyof Score];
-  } else if (elfMove === "Paper" && myMove === "Scissors"
-    || elfMove === "Scissors" && myMove === "Rock"
-    || elfMove === "Rock" && myMove === "Paper") {
-    totalScore += 6 + shapeScore[myMove as keyof Score];
+  const outcome = elfValues[me as keyof ElfEncryption];
+  let myMove = "";
+  if (outcome === "Draw") {
+    totalScore += 3 + shapeScore[elfMove as keyof Score]
+  } else if (outcome === "Lose") {
+    myMove = Lose[elfMove as keyof Moves]
+    totalScore += shapeScore[myMove as keyof Score]
   } else {
-    totalScore += 0 + shapeScore[myMove as keyof Score];
+    myMove = Win[elfMove as keyof Moves]
+    totalScore += 6 + shapeScore[myMove as keyof Score]
   }
 })
+
+console.log(totalScore)
 
 export const answer = totalScore
