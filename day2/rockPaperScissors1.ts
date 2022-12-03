94ec11b6dacbe2ddca81e6a7c7c9e24794abfb4d
johnny2smooth <johnbp13@gmail.com>
2022-12-03 08:58:47 -0800

    prettier formatted

--- day2/rockPaperScissors1.ts
+++ day2/rockPaperScissors1.ts
@@ -1,13 +1,27 @@
 const input = await Deno.readTextFile("day2/input.txt");
-const cleanInput: string[] = input.replaceAll(" ", "").split(/\n/)
+const cleanInput: string[] = input.replaceAll(" ", "").split(/\n/);
 
 export type rockPaperScissors = "Rock" | "Paper" | "Scissors";
 
-export type ElfEncryption = { A: rockPaperScissors, B: rockPaperScissors, C: rockPaperScissors, X: rockPaperScissors, Y: rockPaperScissors, Z: string }
-export type Score = { Rock: number, Paper: number, Scissors: number }
+export type ElfEncryption = {
+  A: rockPaperScissors;
+  B: rockPaperScissors;
+  C: rockPaperScissors;
+  X: rockPaperScissors;
+  Y: rockPaperScissors;
+  Z: string;
+};
+export type Score = { Rock: number; Paper: number; Scissors: number };
 
-const elfValues: ElfEncryption = { "A": "Rock", "B": "Paper", "C": "Scissors", "X": "Rock", "Y": "Paper", "Z": "Scissors" }
-const shapeScore: Score = { "Rock": 1, "Paper": 2, "Scissors": 3 }
+const elfValues: ElfEncryption = {
+  A: "Rock",
+  B: "Paper",
+  C: "Scissors",
+  X: "Rock",
+  Y: "Paper",
+  Z: "Scissors",
+};
+const shapeScore: Score = { Rock: 1, Paper: 2, Scissors: 3 };
 
 let totalScore = 0;
 
@@ -16,13 +30,15 @@ cleanInput.map(([elf, me]: string) => {
   const myMove = elfValues[me as keyof ElfEncryption];
   if (elfMove === myMove) {
     totalScore += 3 + shapeScore[myMove as keyof Score];
-  } else if (elfMove === "Paper" && myMove === "Scissors"
-    || elfMove === "Scissors" && myMove === "Rock"
-    || elfMove === "Rock" && myMove === "Paper") {
+  } else if (
+    (elfMove === "Paper" && myMove === "Scissors") ||
+    (elfMove === "Scissors" && myMove === "Rock") ||
+    (elfMove === "Rock" && myMove === "Paper")
+  ) {
     totalScore += 6 + shapeScore[myMove as keyof Score];
   } else {
     totalScore += 0 + shapeScore[myMove as keyof Score];
   }
-})
+});
 
-export const answer = totalScore
+export const answer = totalScore;
--- day2/rockPaperScissors2.ts
+++ day2/rockPaperScissors2.ts
@@ -1,35 +1,58 @@
 const input = await Deno.readTextFile("day2/input.txt");
-const cleanInput: string[] = input.replaceAll(" ", "").split(/\n/)
+const cleanInput: string[] = input.replaceAll(" ", "").split(/\n/);
 
-export type rockPaperScissors = "Rock" | "Paper" | "Scissors" | "Lose" | "Draw" | "Win";
+export type rockPaperScissors =
+  | "Rock"
+  | "Paper"
+  | "Scissors"
+  | "Lose"
+  | "Draw"
+  | "Win";
 
-export type ElfEncryption = { A: rockPaperScissors, B: rockPaperScissors, C: rockPaperScissors, X: rockPaperScissors, Y: rockPaperScissors, Z: string }
-export type Moves = { Rock: string, Paper: string, Scissors: string }
-export type Score = { Rock: number, Paper: number, Scissors: number }
+export type ElfEncryption = {
+  A: rockPaperScissors;
+  B: rockPaperScissors;
+  C: rockPaperScissors;
+  X: rockPaperScissors;
+  Y: rockPaperScissors;
+  Z: string;
+};
+export type Moves = { Rock: string; Paper: string; Scissors: string };
+export type Score = { Rock: number; Paper: number; Scissors: number };
 
-const elfValues: ElfEncryption = { A: "Rock", B: "Paper", C: "Scissors", X: "Lose", Y: "Draw", Z: "Win" }
-const Lose: Moves = { "Rock": "Scissors", "Paper": "Rock", "Scissors": "Paper" };
-const Win: Moves = { "Rock": "Paper", "Paper": "Scissors", "Scissors": "Rock" };
-const shapeScore: Score = { "Rock": 1, "Paper": 2, "Scissors": 3 };
-
+const elfValues: ElfEncryption = {
+  A: "Rock",
+  B: "Paper",
+  C: "Scissors",
+  X: "Lose",
+  Y: "Draw",
+  Z: "Win",
+};
+const Lose: Moves = { Rock: "Scissors", Paper: "Rock", Scissors: "Paper" };
+const Win: Moves = { Rock: "Paper", Paper: "Scissors", Scissors: "Rock" };
+const shapeScore: Score = { Rock: 1, Paper: 2, Scissors: 3 };
 
 let totalScore = 0;
 
 cleanInput.map(([elf, me]: string) => {
   const elfMove = elfValues[elf as keyof ElfEncryption];
   const outcome = elfValues[me as keyof ElfEncryption];
+  findMyMove(outcome, elfMove);
+});
+
+function findMyMove(outcome: string, elfMove: string) {
   let myMove = "";
   if (outcome === "Draw") {
-    totalScore += 3 + shapeScore[elfMove as keyof Score]
+    totalScore += 3 + shapeScore[elfMove as keyof Score];
   } else if (outcome === "Lose") {
-    myMove = Lose[elfMove as keyof Moves]
-    totalScore += shapeScore[myMove as keyof Score]
+    myMove = Lose[elfMove as keyof Moves];
+    totalScore += shapeScore[myMove as keyof Score];
   } else {
-    myMove = Win[elfMove as keyof Moves]
-    totalScore += 6 + shapeScore[myMove as keyof Score]
+    myMove = Win[elfMove as keyof Moves];
+    totalScore += 6 + shapeScore[myMove as keyof Score];
   }
-})
+}
 
-console.log(totalScore)
+console.log(totalScore);
 
-export const answer = totalScore
+export const answer = totalScore;
