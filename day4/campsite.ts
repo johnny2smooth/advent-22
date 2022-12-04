800f9750590c347cd9bc42ef4ebe0340cea0ab50
johnny2smooth <johnbp13@gmail.com>
2022-12-04 10:53:39 -0800

    this elves are starting to get on my nerves. good thing they're so cute

---
+++ day4/campsite.ts
@@ -0,0 +1,15 @@
+const input = await Deno.readTextFile("day4/input.txt");
+
+const campsiteDuties = input
+  .split(/\n/)
+  .map((input) => input.split(",").map((input) => input.split("-")))
+  .map((areas) => areas.map((areas) => [+areas[0], +areas[1]]))
+  .map((arrays) => [...arrays[0], ...arrays[1]]);
+
+const doesOverlap = campsiteDuties.filter(([start1, end1, start2, end2]) => {
+  return (
+    (start1 <= start2 && end1 >= end2) || (start1 >= start2 && end1 <= end2)
+  );
+}).length;
+
+export default doesOverlap;
