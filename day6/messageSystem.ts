315ec1dacdebcd7ce6cb5584a5067a1da7d89063
johnny2smooth <johnbp13@gmail.com>
2022-12-06 18:12:00 -0800

    second answer found. unused test files deleted

---
+++ day6/messageSystem.ts
@@ -0,0 +1,24 @@
+const input = await Deno.readTextFile("day6/input.txt");
+
+const getCharactersProcessed = (input: string[]) => {
+  const signalSet = new Set();
+  let i = 0;
+  while (signalSet.size <= 14) {
+    if (signalSet.size === 14) {
+      i - 1;
+      break;
+    }
+    if (!signalSet.has(input[i])) {
+      signalSet.add(input[i]);
+      i++;
+    } else {
+      i = i - (signalSet.size - 1);
+      signalSet.clear();
+    }
+  }
+  return i;
+};
+
+getCharactersProcessed(input.split(""));
+
+export default getCharactersProcessed;
